ae7f3c1ad92b4fe3f66becaa09614bb37d2c933c
Vane Quirós P <vquirosp@Vanes-MacBook-Air.local>
2015-03-18 18:26:52 -0600

    item view added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,6 +25,10 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/items', {
+        templateUrl: 'views/items.html',
+        controller: 'ItemsCtrl'
+      })
       .when('/newitem', {
         templateUrl: 'views/newitem.html',
         controller: 'NewitemCtrl'
