51ce4ef8ea75d15da19d6729f22136e1f5ae84be
alvivar <andresalvivar@gmail.com>
2015-03-19 16:23:39 -0600

    ad: Se nos sobrescribe el items del controller, entonces hay que hacer un servicio, jesuita.

--- app/scripts/controllers/items.js
+++ app/scripts/controllers/items.js
@@ -10,35 +10,39 @@
 angular.module('yotestApp')
     .controller('ItemsCtrl', function($scope) {
 
+        // $scope.items = [{
+        //     name: 'Nombre lol 1',
+        //     image: 'http://placekitten.com/g/300/300',
+        //     description: 'Description lol 1 Description lol 1 Description lol 1 Description lol 1 Description lol 1 Description lol 1 '
+        // }, {
+        //     name: 'Nombre lol 2 ',
+        //     image: 'http://placekitten.com/g/300/300',
+        //     description: 'Description lol 2'
+        // }, {
+        //     name: 'Nombre lol 3',
+        //     image: 'http://placekitten.com/g/300/300',
+        //     description: 'Description lol 3'
+        // }, {
+        //     name: 'Nombre lol 4',
+        //     image: 'http://placekitten.com/g/300/300',
+        //     description: 'Description lol 4'
+        // }, {
+        //     name: 'Nombre lol 5',
+        //     image: 'http://placekitten.com/g/300/300',
+        //     description: 'Description lol 5'
+        // }];
 
-        $scope.items = [{
-            name: 'Nombre lol 1',
-            image: 'http://placekitten.com/g/300/300',
-            description: 'Description lol 1 Description lol 1 Description lol 1 Description lol 1 Description lol 1 Description lol 1 '
-        }, {
-            name: 'Nombre lol 2 ',
-            image: 'http://placekitten.com/g/300/300',
-            description: 'Description lol 2'
-        }, {
-            name: 'Nombre lol 3',
-            image: 'http://placekitten.com/g/300/300',
-            description: 'Description lol 3'
-        }, {
-            name: 'Nombre lol 4',
-            image: 'http://placekitten.com/g/300/300',
-            description: 'Description lol 4'
-        }, {
-            name: 'Nombre lol 5',
-            image: 'http://placekitten.com/g/300/300',
-            description: 'Description lol 5'
-        }];
-
+        $scope.items = [];
 
         $scope.AddItem = function() {
 
-            $scope.name = '';
-            $scope.description = '';
-            $scope.image = '';
+            $scope.items.push(
+                $scope.name
+            );
+
+            $scope.name = '<lol></lol>';
+            $scope.description = '<lel></lel>';
+            $scope.image = '<lil></lil>';
         };
 
         $scope.name = '';
