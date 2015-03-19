298c5ab4f4c6a819fe25769666c3d3ae3d1d0414
alvivar <andresalvivar@gmail.com>
2015-03-18 18:51:36 -0600

    ad: Ya se despliegan los datos del controlador de items.

--- app/scripts/controllers/items.js
+++ app/scripts/controllers/items.js
@@ -8,10 +8,28 @@
  * Controller of the yotestApp
  */
 angular.module('yotestApp')
-  .controller('ItemsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('ItemsCtrl', function($scope) {
+
+        $scope.items = [{
+            name: "Nombre lol 1",
+            image: "http://placekitten.com/g/300/300",
+            description: "Description lol 1"
+        }, {
+            name: "Nombre lol 2 ",
+            image: "http://placekitten.com/g/300/300",
+            description: "Description lol 2"
+        }, {
+            name: "Nombre lol 3",
+            image: "http://placekitten.com/g/300/300",
+            description: "Description lol 3"
+        }, {
+            name: "Nombre lol 4",
+            image: "http://placekitten.com/g/300/300",
+            description: "Description lol 4"
+        }, {
+            name: "Nombre lol 5",
+            image: "http://placekitten.com/g/300/300",
+            description: "Description lol 5"
+        }];
+
+    });
